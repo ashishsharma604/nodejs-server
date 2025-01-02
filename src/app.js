922222a2fc9cfd5ce1dc752ebb8c6d94cce5bb4d
euuropa <ashishsharma3816@gmail.com>
2025-01-02 11:54:15 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import the cors middleware
 const routes = require('./routes');
 const authRoutes = require('./routes/auth');
 const pool = require('./db'); // Import the pool from db.js
 
+dotenv.config();
+
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
-dotenv.config();
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use the cors middleware
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', routes);
 app.use('/auth', authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
